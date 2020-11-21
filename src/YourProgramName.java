6b842925c322e0e16b42c266b3dbec82901f2fb3
nicholashui8 <nicholashui8@gmail.com>
2020-11-20 18:31:03 -0800

    initial commit

---
+++ src/YourProgramName.java
@@ -0,0 +1,24 @@
+import ProjectStarterCode.controller.Controller;
+import ProjectStarterCode.controller.Message;
+import ProjectStarterCode.model.Model;
+import ProjectStarterCode.view.View;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class YourProgramName {
+    private static BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
+    private static View view;
+    private static Model model;
+
+    public static void main(String[] args) {
+        view = View.init(queue);
+        model = new Model();
+        Controller controller = new Controller(view, model, queue);
+
+        controller.mainLoop();
+        view.dispose();
+        queue.clear();
+    }
+}
+
